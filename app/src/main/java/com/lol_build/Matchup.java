c05a74e517beff793e6e537d7fe0b708b8b64832
KillianLesp <115100235+9MoiioM9@users.noreply.github.com>
2024-03-24 14:16:00 +0100

    Rename_ activity_main

--- app/src/main/java/com/lol_build/Matchup.java
+++ app/src/main/java/com/lol_build/Matchup.java
@@ -3,12 +3,39 @@ package com.lol_build;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Matchup extends AppCompatActivity {
+
+    public static String Tag = "LolTag";
+
+    private EditText name_input;
+    private Button next_button;
+    private Button exit_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matchup);
+
+        name_input = findViewById(R.id.editTextText);
+        next_button = findViewById(R.id.button);
+        exit_button = findViewById(R.id.button2);
+
+        exit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        next_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO recuperer le nom via le site
+            }
+        });
     }
 }
